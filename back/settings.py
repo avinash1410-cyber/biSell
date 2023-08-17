0a97118b8b0924cfc334a04059f518bfd35ddf95
avinash.kumar1 <avinash.kumar1@spinny.com>
2023-08-17 11:34:38 +0530

    cloudinary issue

--- back/settings.py
+++ back/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = False
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1','https://bishellapi.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
